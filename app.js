99a3c6a421367ce7ffc7ae9932913f800e223104
Shivam <shivamparke@gmail.com>
2022-02-27 20:46:59 -0800

    Final changes to server SQnM

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 "use strict"
 
-// Login endpoint returns access token
-
 const jwtdecode = require('jwt-decode')
 
 let express = require('express')
 const jwtDecode = require("jwt-decode");
+const pathToSwaggerUi = require('swagger-ui-dist').absolutePath()
 let app = express()
+
+app.use(express.static(pathToSwaggerUi))
+
+console.log(pathToSwaggerUi)
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use((request, response,
+/*app.use((request, response,
          nextAction) => {
     if (['https://editor.swagger.io', 'https://hoppscotch.io'].includes(request.headers.origin)) {
         response.header("Access-Control-Allow-Origin", request.headers.origin);
         response.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept");
     }
     nextAction();
-})
+})*/
 
 app.listen(3000, '0.0.0.0')
 console.log('Node.js Express server is running on port 3000...')
@@ -27,8 +30,9 @@ app.get('/v1/hello', get_greeting)
 app.post('/v1/auth', get_token)
 
 let mockTokens = ["eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJTaGl2YW0iLCJpYXQiOjE2NDQxNjg2MDcsImV4cCI6MTY0NDM0MTQwNywiYXVkIjoid3d3Lm9yZWdvbnN0YXRlLmVkdSIsInN1YiI6InNoaXZhbUBvcmVnb25zdGF0ZS5lZHUifQ.BNksseyWZGqruvamD29z9N8b3I_65vJubtHcLY3yN_Q",
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJTaGl2YW0iLCJpYXQiOjE2MTI1NTg1NTEsImV4cCI6MTYxMjY0NDk1MSwiYXVkIjoid3d3Lm9yZWdvbnN0YXRlLmVkdSIsInN1YiI6InNoaXZhbUBvcmVnb25zdGF0ZS5lZHUifQ.EorezPS0RTIDEfk2YkvcN6LfwHzp6PtI7V678-jVzes"]
-let currentMockToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJTaGl2YW0iLCJpYXQiOjE2NDQxNjg2MDcsImV4cCI6MTY0NDM0MTQwNywiYXVkIjoid3d3Lm9yZWdvbnN0YXRlLmVkdSIsInN1YiI6InNoaXZhbUBvcmVnb25zdGF0ZS5lZHUifQ.BNksseyWZGqruvamD29z9N8b3I_65vJubtHcLY3yN_Q"
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJTaGl2YW0iLCJpYXQiOjE2MTI1NTg1NTEsImV4cCI6MTYxMjY0NDk1MSwiYXVkIjoid3d3Lm9yZWdvbnN0YXRlLmVkdSIsInN1YiI6InNoaXZhbUBvcmVnb25zdGF0ZS5lZHUifQ.EorezPS0RTIDEfk2YkvcN6LfwHzp6PtI7V678-jVzes",
+"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJTaGl2YW0iLCJpYXQiOjE2NDYwMTM4MDgsImV4cCI6MTY3NzU0OTgwOCwiYXVkIjoid3d3Lm9yZWdvbnN0YXRlLmVkdSIsInN1YiI6InNoaXZhbUBvcmVnb25zdGF0ZS5lZHUifQ.cnfjuUYlMnHVmxiuZmyK4HAeK3mikjYTgy0mnGTzLTw"]
+let currentMockToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJTaGl2YW0iLCJpYXQiOjE2NDYwMTM4MDgsImV4cCI6MTY3NzU0OTgwOCwiYXVkIjoid3d3Lm9yZWdvbnN0YXRlLmVkdSIsInN1YiI6InNoaXZhbUBvcmVnb25zdGF0ZS5lZHUifQ.cnfjuUYlMnHVmxiuZmyK4HAeK3mikjYTgy0mnGTzLTw"
 let decoded = jwtDecode(currentMockToken)
 let mockTokenExpiry = decoded.exp
 
@@ -94,7 +98,64 @@ function get_weather(request, response) {
         } else {
             return response.sendStatus(401)
         }
-    } else {
+    }
+    // else if (!request.headers.authorization.startsWith("Bearer ")) {
+    //     // TEMPORARY WORKAROUND FOR SWAGGER 2.0
+    //     // https://github.com/OAI/OpenAPI-Specification/issues/583
+    //     let incomingToken = request.headers.authorization
+    //     let incomingTokenExp = jwtDecode(incomingToken).exp * 1000
+    //     if (mockTokens.includes(incomingToken) && incomingTokenExp >= new Date().getTime()) {
+    //         response.json({
+    //             "coord": {
+    //                 "lon": -123.262,
+    //                 "lat": 44.5646
+    //             },
+    //             "weather": [
+    //                 {
+    //                     "id": 802,
+    //                     "main": "Clouds",
+    //                     "description": "scattered clouds",
+    //                     "icon": "03d"
+    //                 }
+    //             ],
+    //             "base": "stations",
+    //             "main": {
+    //                 "temp": 45.14,
+    //                 "feels_like": 43.48,
+    //                 "temp_min": 37.87,
+    //                 "temp_max": 56.34,
+    //                 "pressure": 1020,
+    //                 "humidity": 72,
+    //                 "sea_level": 1020,
+    //                 "grnd_level": 1012
+    //             },
+    //             "visibility": 10000,
+    //             "wind": {
+    //                 "speed": 3.74,
+    //                 "deg": 24,
+    //                 "gust": 5.93
+    //             },
+    //             "clouds": {
+    //                 "all": 44
+    //             },
+    //             "dt": 1642373258,
+    //             "sys": {
+    //                 "type": 1,
+    //                 "id": 5858,
+    //                 "country": "US",
+    //                 "sunrise": 1642347934,
+    //                 "sunset": 1642381185
+    //             },
+    //             "timezone": -28800,
+    //             "id": 5720727,
+    //             "name": "Corvallis",
+    //             "cod": 200
+    //         })
+    //     } else {
+    //         return response.sendStatus(401)
+    //     }
+    // }
+    else {
         response.status(401)
         return response.send("Invalid token format!")
     }
@@ -112,18 +173,18 @@ function get_greeting(request, response) {
         } else {
             return response.sendStatus(401)
         }
-    } else if (!request.headers.authorization.startsWith("Bearer ")) {
-        // TEMPORARY WORKAROUND FOR SWAGGER 2.0
-        // https://github.com/OAI/OpenAPI-Specification/issues/583
-        let incomingToken = request.headers.authorization
-        let incomingTokenExp = jwtDecode(incomingToken).exp * 1000
-        if (mockTokens.includes(incomingToken) && incomingTokenExp >= (new Date().getTime())) {
-            response.json({"greeting": "Hello, from Shivam"})
-        } else {
-            return response.sendStatus(401)
-        }
     }
-
+    // else if (!request.headers.authorization.startsWith("Bearer ")) {
+    //     // TEMPORARY WORKAROUND FOR SWAGGER 2.0
+    //     // https://github.com/OAI/OpenAPI-Specification/issues/583
+    //     let incomingToken = request.headers.authorization
+    //     let incomingTokenExp = jwtDecode(incomingToken).exp * 1000
+    //     if (mockTokens.includes(incomingToken) && incomingTokenExp >= (new Date().getTime())) {
+    //         response.json({"greeting": "Hello, from Shivam"})
+    //     } else {
+    //         return response.sendStatus(401)
+    //     }
+    // }
     else {
         response.status(400)
         return response.send("Invalid token format!")
@@ -144,7 +205,7 @@ function get_token(request, response) {
 
     //response.send(`${request.body.username} is logged in! Here is your mock token: ${mockToken}`)
     response.json({
-        "access-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJTaGl2YW0iLCJpYXQiOjE2NDQxNjg2MDcsImV4cCI6MTY0NDM0MTQwNywiYXVkIjoid3d3Lm9yZWdvbnN0YXRlLmVkdSIsInN1YiI6InNoaXZhbUBvcmVnb25zdGF0ZS5lZHUifQ.BNksseyWZGqruvamD29z9N8b3I_65vJubtHcLY3yN_Q",
-        "expires": "2022-02-08T17:30:07.000Z"
+        "access-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJTaGl2YW0iLCJpYXQiOjE2NDYwMTM4MDgsImV4cCI6MTY3NzU0OTgwOCwiYXVkIjoid3d3Lm9yZWdvbnN0YXRlLmVkdSIsInN1YiI6InNoaXZhbUBvcmVnb25zdGF0ZS5lZHUifQ.cnfjuUYlMnHVmxiuZmyK4HAeK3mikjYTgy0mnGTzLTw",
+        "expires": "2023-02-28T02:03:28.561Z"
     })
 }
